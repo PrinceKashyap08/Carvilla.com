e5a1dacb6e6a9f073177c9a9eccf427c8de58246
Prince <vasukashyap08@gmail.com>
2023-12-11 20:51:11 +0530

    commit

--- index.js
+++ index.js
@@ -11,4 +11,42 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu(){
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-}
+};
+
+$('#client .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 40,
+    nav: false,
+    autoplay: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 40,
+    nav: false,
+    autoplay: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})

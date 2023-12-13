ee4adc7d050e0a70c59d667317df57db02da826a
Prince <vasukashyap08@gmail.com>
2023-12-13 16:47:03 +0530

    commit

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function mobileMenu(){
 $('#client .owl-carousel').owlCarousel({
     loop: true,
     margin: 30,
-    nav: true,
+    nav: false,
+    dots: false,
     autoplay: true,
     responsive: {
         0: {
@@ -36,19 +37,41 @@ $('#client .owl-carousel').owlCarousel({
 // owlCarousel 
 $( ' #carBrand .owl-carousel').owlCarousel({
     loop: true,
-    margin: 30,
+    margin: 20,
     nav: false,
     dots: false,
     autoplay: true,
     responsive: {
         0: {
-            items: 1
+            items: 2
         },
         600: {
-            items: 2
+            items: 4
         },
         1000: {
             items: 6
+        }
+    }
+});
+// owlCarousel 
+$( ' #newCars .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
         }
     }
 });
